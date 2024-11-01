491aa30541aeab44cd48952f3534e9e2dbaa663f
syh213 <syh21133@gmail.com>
2024-11-01 17:37:19 +0900

    feat : 패키지 설정

---
+++ src/main/java/com/sparta/outsorucing/domain/order/controller/OrderController.java
@@ -0,0 +1,12 @@
+package com.sparta.outsorucing.domain.order.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/orders")
+public class OrderController {
+
+}
---
+++ src/main/java/com/sparta/outsorucing/domain/order/service/OrderService.java
@@ -0,0 +1,10 @@
+package com.sparta.outsorucing.domain.order.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class OrderService {
+
+}
