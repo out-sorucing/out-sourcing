d057905423950445f2ed0b1d412cbcf49bcec209
kuk1157 <kuk1938@naver.com>
2024-11-04 22:13:24 +0900

    feat: 토큰 연동 - 가게 생성 3개까지(본인아이디 기준) - 가게 수정(본인만) - 가게 폐업(본인만)

--- src/main/java/com/sparta/outsorucing/domain/store/repository/StoreRepository.java
+++ src/main/java/com/sparta/outsorucing/domain/store/repository/StoreRepository.java
@@ -4,10 +4,16 @@ import com.sparta.outsorucing.domain.store.entity.Store;
 
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface StoreRepository extends JpaRepository<Store, Long> {
+
     //  가게 검색
     List<Store> findAllByStoreNameContainsOrderByIdDesc(String storeName);
+
+    // 가게 3개 개수 체크
+    int countByMemberId(Long memberId);
+
 }
