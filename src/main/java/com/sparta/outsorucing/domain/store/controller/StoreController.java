818798aef5e4dc87e608d8607b80df1b4ef323af
kuk1157 <kuk1938@naver.com>
2024-11-04 15:32:59 +0900

    feat: 가게 전체 목록조회 - 추후에 토큰정보 받는 형태로 변경

--- src/main/java/com/sparta/outsorucing/domain/store/controller/StoreController.java
+++ src/main/java/com/sparta/outsorucing/domain/store/controller/StoreController.java
@@ -4,7 +4,10 @@ import com.sparta.outsorucing.domain.store.dto.StoreRequestDto;
 import com.sparta.outsorucing.domain.store.dto.StoreResponseDto;
 import com.sparta.outsorucing.domain.store.service.StoreService;
 import jakarta.validation.Valid;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,5 +24,12 @@ public class StoreController {
     public StoreResponseDto createStore(@RequestBody @Valid StoreRequestDto requestDto) {
         return storeService.createStore(requestDto);
     }
+
+    // 전체 가게목록 조회(소비자 입장 화면)
+    @GetMapping
+    public List<StoreResponseDto> findStore(){
+        return storeService.findStore();
+    }
+
 
 }
