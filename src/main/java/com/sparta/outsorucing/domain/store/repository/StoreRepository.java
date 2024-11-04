ee46664f8e237922ab1fed285a9140bc1f2fbcbe
syh213 <syh21133@gmail.com>
2024-11-04 12:59:09 +0900

    feat : 주문 요청 기능

--- src/main/java/com/sparta/outsorucing/domain/store/repository/StoreRepository.java
+++ src/main/java/com/sparta/outsorucing/domain/store/repository/StoreRepository.java
@@ -1,7 +1,6 @@
 package com.sparta.outsorucing.domain.store.repository;
 
 import com.sparta.outsorucing.domain.store.entity.Store;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
