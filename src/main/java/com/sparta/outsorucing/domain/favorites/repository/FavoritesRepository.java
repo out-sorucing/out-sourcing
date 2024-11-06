8ac58302bab9fb355af6240bad60d8765d2c5499
kuk1157 <kuk1938@naver.com>
2024-11-06 19:47:18 +0900

    refactor: 가게관리 및 즐겨찾기 관리 수정

--- src/main/java/com/sparta/outsorucing/domain/favorites/repository/FavoritesRepository.java
+++ src/main/java/com/sparta/outsorucing/domain/favorites/repository/FavoritesRepository.java
@@ -7,4 +7,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface FavoritesRepository extends JpaRepository<Favorites, Long> {
     List<Favorites> findByMemberId(Long memberId);
     int countByStoreIdAndMemberId(Long storeId, Long memberId);
+
+    // 즐겨찾기 본인 즐겨찾기 삭제 체크
+    int countByMemberIdAndId(Long memberId, Long id);
+
 }
